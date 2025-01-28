00b1a2d943e8bd193151d25f123c417645dd23a3
julienL79 <loiseau.julien79@gmail.com>
2025-01-28 12:05:25 +0100

    update

--- 3. TS avec React/Projet_react_TS/src/components/common/molecules/Navbar/Navbar.tsx
+++ 3. TS avec React/Projet_react_TS/src/components/common/molecules/Navbar/Navbar.tsx
@@ -4,13 +4,13 @@ import "./Navbar.css"
 
 export const Navbar = () => {
     return(
-        <>
+        <div className="navbar">
             <Link className={"navbar-logo"} href={"#"} content={
                 <Image className={"logo"} src={"./logo.png"} alt={"logo"}/>
             }/>
             <Link className={"navbar-link"} href={"#"} content={'Home'}/>
             <Link className={"navbar-link"} href={"#"} content={'About'}/>
             <Link className={"navbar-link"} href={"#"} content={'My space'}/>
-        </>
+        </div>
     )
 }
